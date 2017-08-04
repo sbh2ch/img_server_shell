7f5fd125741ab02f915670e8a348b439aad621d2
root <sbh2ch@gmail.com>
2017-08-04 16:41:42 +0900

    update gitignore and scripts

---
+++ getName.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+
+FILENAME=`find /home/dev/originalImages/2017/08/04 -name COMS_GOCI_BI_2017080401*.JPG`
+
+echo "$FILENAME"
+echo "complete"
+
--- get_transfer.sh
+++ get_transfer.sh
@@ -2,39 +2,54 @@
 # 파일이 존재하지 않을 시 FTP로 파일 들고오기.
 # FTP로도 없을 시 메일 알림 및 10분 후 재시도
 
-# 파일 유무 체크, 있을시 1 반환 없을시 0 반환
+YY=`date '+%Y'`
+MM=`date '+%m'`
+DD=`date '+%d'`
+TIME=01
+
+# 파일 유무 체크, 있을시 1, 없을시 0
 fileCheck(){
-  TIME=$1
-  YY=`date '+%Y'`
-  MM=`date '+%m'`
-  DD=`date '+%d'`
-  
   ls /home/dev/originalImages/$YY/$MM/$DD | grep COMS_GOCI_BI_$YY$MM$DD$TIME\
   && return 1\
   || return 0;
 }
 
+# 파일 분할
 divide() {
-  echo "divide it!"
+  FILENAME=`find /home/dev/originalImages/$YY/$MM/$DD -name COMS_GOCI_BI_$YY$MM$DD$TIME*.JPG`
+  echo "divide [[ $FILENAME ]]"
 }
 
+# for loop test
+forloop() {
+  for((i=0; $i <= 5; i++))
+  do
+    echo "$i"
+  done
+}
+
+# 경고 메일 발송
 sendAlertEmail() {
   echo "send a mail"
+  echo "COMS_GOCI_BI_$YY$MM$DD$TIME [[RGB]] is not exist!" | mail -s "COMS_GOCI_BI_$YY$MM$DD$TIME [[RGB]] is not exist!" sbh2ch@gmail.com byunghwa.son@kiost.ac.kr
 }
 
+# FTP로 자료 끌어오기
 getFromFTP() {
   echo "ftp"
 }
 
-fileCheck 00
+forloop
+
+fileCheck
 if [ "$?" -eq "1" ]; then
   echo "ok"
   divide
 else
   echo "no such file exception"
   echo "try to get JPG file via FTP"
-  getFromFTP 01
-  fileCheck 00
+  getFromFTP
+  fileCheck
   if [ "$?" -eq "1" ]; then
     echo "ok"
     divide
@@ -46,7 +61,7 @@ else
     sleep 1
     echo "1"
     sleep 1
-    fileCheck 00
+    fileCheck
     if [ "$?" -eq "1" ]; then
       echo "ok"
       divide
